55f40be524083bfb1dfa2d3fc18a3fbe7a9b4aff
yunju <dbswnfnal@naver.com>
2024-09-02 09:48:22 +0900

    fix : 여행, 코스 RestController로 변경

--- totreviews/src/main/java/totreviews/controller/CourseRestController.java
+++ totreviews/src/main/java/totreviews/controller/CourseRestController.java
@@ -1,24 +1,22 @@
 package totreviews.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import totreviews.domain.CourseDTO;
 import totreviews.service.CourseService;
 
-@Controller
+@RestController
 @RequestMapping("/course")
-public class CourseController {
+public class CourseRestController {
 
 	@Autowired
 	private CourseService courseService;
 
 	@GetMapping("/{courseId}")
-	@ResponseBody
 	public CourseDTO getCourseById(@PathVariable("courseId") String courseId) {
 		return courseService.getCourseById(courseId);
 	}
--- totreviews/src/main/java/totreviews/controller/TourRestController.java
+++ totreviews/src/main/java/totreviews/controller/TourRestController.java
@@ -1,24 +1,22 @@
 package totreviews.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import totreviews.domain.TourDTO;
 import totreviews.service.TourService;
 
-@Controller
+@RestController
 @RequestMapping("tour")
-public class TourController {
+public class TourRestController {
 
 	@Autowired
 	private TourService tourService;
 
 	@GetMapping("/{tourId}")
-	@ResponseBody
 	public TourDTO getTourById(@PathVariable("tourId") String tourId) {
 		return tourService.getTourById(tourId);
 	}
